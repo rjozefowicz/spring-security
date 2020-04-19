ff2f405a95a473f05bf1ed937e14bb1734f262e5
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2020-04-19 13:49:56 +0200

    userdetailsservice - full impl

--- day1/form-login-2/src/main/java/dev/jozefowicz/springsecurity/formlogin2/configuration/SecurityConfiguration.java
+++ day1/form-login-2/src/main/java/dev/jozefowicz/springsecurity/formlogin2/configuration/SecurityConfiguration.java
@@ -1,17 +1,30 @@
 package dev.jozefowicz.springsecurity.formlogin2.configuration;
 
+import dev.jozefowicz.springsecurity.formlogin2.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserRepository userRepository;
+
+    public SecurityConfiguration(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -21,14 +34,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
-                .inMemoryAuthentication()
-                    .withUser("jan")
-                    .password("kowalski")
-                    .authorities(Collections.emptyList())
-                .and()
-                    .withUser("stefan")
-                    .password("kisielewski")
-                    .authorities(Collections.emptyList());
+                .userDetailsService(username -> {
+                    final UserRepository.User user =
+                            userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(""));
+                    return new User(
+                            user.getUsername(),
+                            user.getPassword(),
+                            user.getRoles().stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+                });
     }
 
     @Override
