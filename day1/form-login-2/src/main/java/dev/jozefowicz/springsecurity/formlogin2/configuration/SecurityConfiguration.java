6f86113c807c06f697e10d4d64eab9614ce51f54
Radek Józefowicz <radoslawjozefowicz@gmail.com>
2023-11-03 00:25:34 +0100

    migrating form-login-2 and multiple-passwords-encoders to spring boot 3

--- day1/form-login-2/src/main/java/dev/jozefowicz/springsecurity/formlogin2/configuration/SecurityConfiguration.java
+++ day1/form-login-2/src/main/java/dev/jozefowicz/springsecurity/formlogin2/configuration/SecurityConfiguration.java
@@ -1,14 +1,17 @@
 package dev.jozefowicz.springsecurity.formlogin2.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) {
-       // FIXME
-    }
+    @Bean
+   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+       return httpSecurity.build();
+   }
 }
--- day1/multiple-password-encoders/src/main/java/dev/jozefowicz/springsecurity/multiplepasswordencoders/configuration/SecurityConfiguration.java
+++ day1/multiple-password-encoders/src/main/java/dev/jozefowicz/springsecurity/multiplepasswordencoders/configuration/SecurityConfiguration.java
@@ -1,21 +1,25 @@
 package dev.jozefowicz.springsecurity.multiplepasswordencoders.configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
 @EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests(authorizeRequests -> {
                     authorizeRequests
                             .anyRequest()
                             .authenticated();
                 })
-                .formLogin();
+                .formLogin(Customizer.withDefaults());
+        return http.build();
     }
-
 }
